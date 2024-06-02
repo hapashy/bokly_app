be820942b5c55a82ed6c5e31c841cb111ed445e9
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-02 17:04:17 +0300

    create newest books cubit

---
+++ lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bokly_app/Features/home/domain/entitys/book_entity.dart';
+import 'package:bokly_app/Features/home/domain/use_cases/fetch_news_books.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+
+    var result = await fetchNewestBooksUseCase.call();
+
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.message));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
